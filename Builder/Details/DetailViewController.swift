0a251be18dccde6dae54a7b44cbd1bf4d226d317
Michael Long <hmlong@gmail.com>
2021-11-28 16:40:31 -0600

    onReceive demo code

--- Builder/Details/DetailViewController.swift
+++ Builder/Details/DetailViewController.swift
@@ -60,8 +60,8 @@ class DetailViewController: UIViewController {
             }
             .padding(20)
             .spacing(20)
-            .onReceive(viewModel.$accepted) { _, value in
-                print(value)
+            .onReceive(viewModel.$accepted) { context, value in
+                context.view.accessibilityLabel = value ? "Terms accepted." : "Terms Not yet accepted."
             }
         }
         .backgroundColor(.quaternarySystemFill)
